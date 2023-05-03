364d1d5f2a0763c9c3d83fcdef2b8b4640e7111a
mrrobot331 <ivan126ns@gmail.com>
2023-05-03 11:47:41 +0200

    dasda

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -47,7 +47,7 @@ display: flex;
 flex-direction: column;
 justify-content: space-between;
 align-items: center;
-background-image: ${props=>`url("../../images/${props.bgImage}")`}
+background-image: ${props=>`url("../../${props.bgImage}")`}
 `
 const ItemText=styled.div`
 padding-top:15vh;
